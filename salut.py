5d6329b3aa81afcc4ad7e7d910f7edaacefb4831
lucanchling <luc.anchling@cpe.fr>
2020-11-18 08:42:34 +0100

    Création salut.py

---
+++ salut.py
@@ -0,0 +1,13 @@
+# Je suis un bout de code python
+
+salut=[]
+
+def crea(liste):
+    for i in range(50):
+        if i%2==0:
+            liste.append(1)
+        else:
+            liste.append(0)
+    return liste
+
+print(crea(salut))
